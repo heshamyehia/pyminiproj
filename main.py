0d0fe78174d47106491cf951c12f5eac50ab9af9
heshamyehia <120581682+heshamyehia@users.noreply.github.com>
2025-12-18 14:23:28 +0200

    feat: added main to integrate package usage

---
+++ main.py
@@ -0,0 +1,22 @@
+from dataframe_pcg import Dataframe,stats
+
+
+def main():
+    # TODO: Read data
+    
+    datapath="data/titanic.csv"
+    dtype="data/titanic_dtype.csv"
+    
+    df = Dataframe.read_csv(datapath, dtype) 
+    # TODO: Fill missing values
+    df.fillna(stats.get_col_mean, stats.get_col_mode)
+    print(df.count_nulls())
+    # Numeric columns → mean
+    # Categorical columns → mode
+    # TODO:Generate statistics file
+    df.describe()
+    # TODO:Write cleaned data to CSV
+    df.to_csv("data/titanic_cleaned.csv")
+
+if __name__ == "__main__":
+    main()
